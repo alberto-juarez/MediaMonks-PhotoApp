a800dcc75bf0fbb1ee6f33031d56498ef71f4818
Alberto Juarez <aji147@gmail.com>
2021-01-18 16:05:22 -0600

    Initial Commit

---
+++ MediaMonks-PhotoApp/MediaMonks_PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaMonks_PhotoAppApp.swift
+//  MediaMonks-PhotoApp
+//
+//  Created by Alberto Juarez on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MediaMonks_PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaMonks-PhotoAppTests/MediaMonks_PhotoAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  MediaMonks_PhotoAppTests.swift
+//  MediaMonks-PhotoAppTests
+//
+//  Created by Alberto Juarez on 18/01/21.
+//
+
+import XCTest
+@testable import MediaMonks_PhotoApp
+
+class MediaMonks_PhotoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
